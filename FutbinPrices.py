355271bf187571eae6924b6fcb169e966aeb7e17
Rafael Garrido <rafaelgarrido@gmail.com>
2019-01-07 23:16:01 -0500

    Update FutbinPrices.py

--- FutbinPrices.py
+++ FutbinPrices.py
@@ -19,7 +19,6 @@ def fetch_price():
         response = requests.get(url)
         data = response.json()
         current_px[name] = data[str(id)]['prices']['ps']['LCPrice']
-    return current_px
 
 fetch_price()
 print(current_px, 'Kick off')
@@ -29,15 +28,18 @@ for (name, id) in player_id.items():
 while True:
         current_time = datetime.datetime.now()
         fetch_price()
-        if old_px == current_px:
-                print('-'*60)
-                print('\33[0;37;40m No price updates detected')
-                print(current_px, current_time.strftime("%H:%M %Z"))
-        else:
-                old_px = current_px
-                print('*'*60)
-                print('\33[1;31;40m Price updates detected. Check list closely!!!!!!!!!!!!!')
-                print('*'*60)
-                print(current_px,current_time.strftime("%H:%M %Z"))
-        
+        for (name, id) in player_id.items():
+                player_name = name
+                old_price = old_px[name]
+                current_price = current_px[name]
+                player_changed = False
+                if old_price != current_price:
+                        player_changed = True
+                if not player_changed:
+                        print('-'*60)
+                        print('\33[0;37;40m No price updates detected for', playner_name, '=>', current_px, current_time.strftime("%H:%M %Z"))
+                else:
+                        old_px[name] = current_px[name]
+                        print('*'*60)
+                        print('\33[1;31;40m Price updates detected for', player_name, ' from:', old_price, 'to:', current_price, 'at', current_time.strftime("%H:%M %Z"))
         time.sleep(10)
